3a2130cc9ebd4cb92933b3ff3c95e04a4720684d
Sushil Kumawat <tony281723@gmail.com>
2024-11-25 08:09:47 +0530

    learning zod

--- mistrymessage/src/schemas/messageSchema.ts
+++ mistrymessage/src/schemas/messageSchema.ts
@@ -1,1 +1,6 @@
- 
+import {z} from "zod"
+
+
+export const messageSchema = z.object({
+    content: z.string().min(10,{message:'Content must be at least 10 characters'}).max(300,{message:'Content must be at no longer then  300 characters'})
+})
