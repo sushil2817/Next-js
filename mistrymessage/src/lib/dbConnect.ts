1eb37734b82f073ac9caddd415ab343f105678ec
sushil2817 <tony281723@gmail.com>
2024-12-11 22:04:05 +0530

    next js project

--- mistrymessage/src/lib/dbConnect.ts
+++
@@ -1,5 +0,0 @@
-import mongoose  from "mongoose";
-
-type connectionObject = {
-    isConnected?: number
-}
--- mistrymessage/src/schemas/messageSchema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod"
-
-
-export const signInSchema = z.object({
-    content:z
-    .string()
-    .min(10,{message:"content  must be at least of 10 characters"})
-    .max(300,{message:"content must be no longer then 300 characters"})
-})
